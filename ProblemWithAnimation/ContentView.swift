609b5796740abb97d551a42c1f12dba9f01506ee
Pavel Pudovkin <pudovkin1996@gmail.com>
2024-04-28 14:22:29 +0300

    Initial Commit

---
+++ ProblemWithAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProblemWithAnimation
+//
+//  Created by Павел on 28.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProblemWithAnimation/ProblemWithAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProblemWithAnimationApp.swift
+//  ProblemWithAnimation
+//
+//  Created by Павел on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProblemWithAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
